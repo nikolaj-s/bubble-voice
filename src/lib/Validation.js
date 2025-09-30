f84cd169b8df10f6cb70ba8244398d2a39dc90a2
Nikolaj Whitesage <nikolaj@Mac.ht.home>
2025-09-30 16:03:01 -0700

    added dev env bool

--- src/lib/Validation.js
+++ src/lib/Validation.js
@@ -1,5 +1,5 @@
 
-const URL = 'http://localhost:3016';
+const URL = process.env.REACT_APP_DEV ? 'http://localhost:3016' : 'https://bubblechat.net';
 
 export const INVITE_URL = `${URL}/bubble-invite`
 
