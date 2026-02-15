6a4ae904bd5b758e6b48f5e8e777da5cce3eaa6c
Nikolaj <niko.sage@gmail.com>
2026-02-14 18:48:48 -0800

    misc windows changes

--- src/layout/Overlays/ScreenPickerOverlay/ScreenPickerOverlay.js
+++ src/layout/Overlays/ScreenPickerOverlay/ScreenPickerOverlay.js
@@ -3,17 +3,15 @@ import { ScreenShare } from 'lucide-react';
 import ContentHeader from '../../../components/Headers/ContentHeader/ContentHeader';
 import ScreenPicker from '../../../components/ScreenPicker/ScreenPicker';
 import ScrollLoadWrapper from '../../../components/ui/Wrappers/ScrollLoadWrapper/ScrollLoadWrapper';
-import { MenuCloseHeader } from '../../../components/Headers/MenuCloseHeader/MenuCloseHeader';
 
 export const ScreenPickerOverlay = ({close}) => {
 
     return (
-        <div style={{backgroundColor: 'var(--primary-color)', display: 'grid', gridTemplateRows: '45px auto', borderRadius: 'var(--border-radius)', overflow: 'hidden'}}>
-            <MenuCloseHeader title={'Choose A Stream'} onClose={close} />
-                    <ScrollLoadWrapper>
-                    <ContentHeader Icon={ScreenShare} title={"Start Streaming"} subTitle={'Choose a source below to begin streaming'} />
-                    <ScreenPicker />
-                    </ScrollLoadWrapper>
-        </div>
+        <>
+        <ScrollLoadWrapper>
+        <ContentHeader Icon={ScreenShare} title={"Start Streaming"} subTitle={'Choose a source below to begin streaming'} />
+        <ScreenPicker />
+        </ScrollLoadWrapper>
+        </>
     )
 }
